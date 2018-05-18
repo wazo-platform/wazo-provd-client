a4a9443f4dad7e312ad9c53cc6f2ec9901022814
Francois Blackburn <fblackburn@wazo.community>
2018-05-18 13:12:36 -0400

    config_manager: fix autocreate method

--- wazo_provd_client/commands/config_manager.py
+++ wazo_provd_client/commands/config_manager.py
@@ -1,7 +1,8 @@
 # Copyright 2018 The Wazo Authors  (see AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import json, base64
+import base64
+import json
 
 from wazo_provd_client.command import ProvdCommand
 
@@ -12,56 +13,51 @@ class ConfigManagerCommand(ProvdCommand):
         'Content-Type': 'application/vnd.proformatique.provd+json'
     }
 
-    def __init__(self, client):
-        super().__init__(client)
-        self.base_url = '{}/configs'.format(self.base_url)
-
     def list_registrar(self, **params):
+        url = '{base}/configs'.format(base=self.base)
         params.update(self._prepare_query({'X_type': 'registrar'}))
-        r = self.session.get(self.base_url, params=params)
+        r = self.session.get(url, params=params)
         self.raise_from_response(r)
         return r.json()
 
     def list_device(self, **params):
+        url = '{base}/configs'.format(base=self.base)
         params.update(self._prepare_query({'X_type': 'device'}))
-        r = self.session.get(self.base_url, params=params)
+        r = self.session.get(url, params=params)
         self.raise_from_response(r)
         return r.json()
 
-    def get(self, id):
-        r = self.session.get('{}/{}'.format(self.base_url, id),
-                             headers=self._headers)
+    def get(self, id_):
+        url = '{base}/configs/{id_}'.format(base=self.base, id_=id_)
+        r = self.session.get(url, headers=self._headers)
         if r.status_code != 200:
             self.raise_from_response(r)
 
         return r.json()
 
     def create(self, data):
-        r = self.session.post(self.base_url,
-                              data=json.dumps({'config': data}),
-                              headers=self._headers)
+        url = '{base}/configs'.format(base=self.base)
+        r = self.session.post(url, data=json.dumps({'config': data}), headers=self._headers)
         if r.status_code != 201:
             self.raise_from_response(r)
 
         return r.json()
 
-    def update(self, id, data):
-        r = self.session.put('{}/{}'.format(self.base_url, id),
-                             data=json.dumps({'config': data}),
-                             headers=self._headers)
+    def update(self, id_, data):
+        url = '{base}/configs/{id_}'.format(base=self.base, id_=id_)
+        r = self.session.put(url, data=json.dumps({'config': data}), headers=self._headers)
         if r.status_code != 204:
             self.raise_from_response(r)
 
-    def delete(self, id):
-        r = self.session.put('{}/{}'.format(self.base_url, id),
-                             headers=self._headers)
+    def delete(self, id_):
+        url = '{base}/configs/{id_}'.format(base=self.base, id_=id_)
+        r = self.session.put(url, headers=self._headers)
         if r.status_code != 204:
             self.raise_from_response(r)
 
     def autocreate(self):
-        r = self.session.post('{}/autocreate'.format(self.base_url),
-                              data=json.dumps({}),
-                              headers=self._headers)
+        url = '{base}/autocreate'.format(base=self.base)
+        r = self.session.post(url, data=json.dumps({}), headers=self._headers)
         if r.status_code != 201:
             self.raise_from_response(r)
 
