5ad61dd555f772cca1b42164c088e77bd6143cad
Francois Blackburn <fblackburn@wazo.community>
2018-05-18 13:22:11 -0400

    standardize commands

--- wazo_provd_client/commands/config_manager.py
+++ wazo_provd_client/commands/config_manager.py
@@ -30,37 +30,29 @@ class ConfigManagerCommand(ProvdCommand):
     def get(self, id_):
         url = '{base}/configs/{id_}'.format(base=self.base, id_=id_)
         r = self.session.get(url, headers=self._headers)
-        if r.status_code != 200:
-            self.raise_from_response(r)
-
+        self.raise_from_response(r)
         return r.json()
 
     def create(self, data):
         url = '{base}/configs'.format(base=self.base)
         r = self.session.post(url, data=json.dumps({'config': data}), headers=self._headers)
-        if r.status_code != 201:
-            self.raise_from_response(r)
-
+        self.raise_from_response(r)
         return r.json()
 
     def update(self, id_, data):
         url = '{base}/configs/{id_}'.format(base=self.base, id_=id_)
         r = self.session.put(url, data=json.dumps({'config': data}), headers=self._headers)
-        if r.status_code != 204:
-            self.raise_from_response(r)
+        self.raise_from_response(r)
 
     def delete(self, id_):
         url = '{base}/configs/{id_}'.format(base=self.base, id_=id_)
         r = self.session.put(url, headers=self._headers)
-        if r.status_code != 204:
-            self.raise_from_response(r)
+        self.raise_from_response(r)
 
     def autocreate(self):
         url = '{base}/autocreate'.format(base=self.base)
         r = self.session.post(url, data=json.dumps({}), headers=self._headers)
-        if r.status_code != 201:
-            self.raise_from_response(r)
-
+        self.raise_from_response(r)
         return r.json()
 
     def _prepare_query(self, query):
--- wazo_provd_client/commands/plugin_manager.py
+++ wazo_provd_client/commands/plugin_manager.py
@@ -12,39 +12,29 @@ class PluginManagerCommand(ProvdCommand):
         'Content-Type': 'application/vnd.proformatique.provd+json'
     }
 
-    def __init__(self, client):
-        super().__init__(client)
-        self.base_url = '{}/install'.format(self.base_url)
-
     def update(self):
-        r = self.session.post('{}/update'.format(self.base_url),
-                              data=json.dumps({}),
-                              headers=self._headers)
-        if r.status_code != 201:
-            self.raise_from_response(r)
+        url = '{base}/install/update'.format(base=self.base)
+        r = self.session.post(url, data=json.dumps({}), headers=self._headers)
+        self.raise_from_response(r)
 
     def install(self, id):
-        r = self.session.post('{}/install'.format(self.base_url),
-                              data=json.dumps({'id': id}),
-                              headers=self._headers)
-        if r.status_code != 201:
-            self.raise_from_response(r)
+        url = '{base}/install/install'.format(base=self.base)
+        r = self.session.post(url, data=json.dumps({'id': id}), headers=self._headers)
+        self.raise_from_response(r)
 
     def uninstall(self, id):
-        r = self.session.post('{}/uninstall'.format(self.base_url),
-                              data=json.dumps({'id': id}),
-                              headers=self._headers)
-        if r.status_code != 201:
-            self.raise_from_response(r)
+        url = '{base}/install/uninstall'.format(base=self.base)
+        r = self.session.post(url, data=json.dumps({'id': id}), headers=self._headers)
+        self.raise_from_response(r)
 
     def list_installed(self, **params):
-        r = self.session.get('{}/installed'.format(self.base_url),
-                             params=params)
+        url = '{base}/install/installed'.format(base=self.base)
+        r = self.session.get(url, params=params)
         self.raise_from_response(r)
         return r.json()
 
     def list_installable(self, **params):
-        r = self.session.get('{}/installable'.format(self.base_url),
-                             params=params)
+        url = '{base}/install/installable'.format(base=self.base)
+        r = self.session.get(url, params=params)
         self.raise_from_response(r)
         return r.json()
