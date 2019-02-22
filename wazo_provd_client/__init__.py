6094a8a7778945b7ac13ecb39e49c54dd7408c8d
Alexandre Fournier <afournier@wazo.io>
2019-02-22 16:46:06 -0500

    update SPDX-License-Identifier

--- wazo_provd_client/__init__.py
+++ wazo_provd_client/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2018 The Wazo Authors  (see AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_provd_client.client import Client
 
