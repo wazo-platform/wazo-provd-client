291c61a831232e4a7304e059e0ae4908b5be4630
Francois Blackburn <fblackburn@wazo.community>
2020-06-10 12:04:04 -0400

    update default config to use prefix on 443

--- wazo_provd_client/client.py
+++ wazo_provd_client/client.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.client import BaseClient
@@ -9,11 +9,13 @@ class Client(BaseClient):
 
     def __init__(self,
                  host,
-                 port=8666,
+                 port=443,
+                 prefix='/api/provd',
                  version='0.2',
                  **kwargs):
         super(Client, self).__init__(
             host=host,
             port=port,
+            prefix=prefix,
             version=version,
             **kwargs)
